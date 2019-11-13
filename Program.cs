8ed1b41109f9b4c63f8d39b1a5792d193f872ff9
TristanCho <51831659+TristanCho@users.noreply.github.com>
2019-11-14 00:29:32 +0100

    00:29

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 namespace Constructores
 {
     //CONSTRUCTORES -- PILDORAS INFORMÁTICAS VIDEO 30 --
-    //  https://www.youtube.com/watch?v=FobGQKXVpH0&list=PLU8oAlHdN5BmpIQGDSHo5e1r4ZYWQ8m4B&index=30
+    //  https://www.youtube.com/watch?v=FobGQKXVpH0&list=PLU8oAlHdN5BmpIQGDSHo5e1r4ZYWQ8m4B&index=30 //VT30 Constructores 
+    //  https://www.youtube.com/watch?v=7HGwVYsdAxI&list=PLU8oAlHdN5BmpIQGDSHo5e1r4ZYWQ8m4B&index=31 //GETTER Y SETTER
     class Program
     {
         static void Main(string[] args)
@@ -29,13 +30,14 @@ namespace Constructores
     }
     class Coche
     {
-        
+
         public Coche() //Este es el constructor que no es ni void ni devuelve nada es un metoddo especial - El constructor debe tener el mismo nombre de la clase
-            //Todas las clases tienen un constructor aunque no se vea por defecto estará invocando al constructor por defecto, en este caso public Coche(){ruedas=...}...
+                       //Todas las clases tienen un constructor aunque no se vea por defecto estará invocando al constructor por defecto, en este caso public Coche(){ruedas=...}...
         {
             ruedas = 4;
-            largo= 2300.5;
+            largo = 2300.5;
             ancho = 0.800;
+            //climatizador = true;
         }
 
         public Coche(double largoCoche, double anchoCoche)
@@ -47,20 +49,21 @@ namespace Constructores
 
         public String getInfoCoche()
         {
-            
-            return "Información del coche: \nRuedas: "+ ruedas + " \nLargo: " + largo + "\nAncho: " + ancho + "\n";
+
+            return "Información del coche: \nRuedas: " + ruedas + " \nLargo: " + largo + "\nAncho: " + ancho + "\n";
         }
-        
+
+        public void setExtras(bool paramClimatizador, String paramTapiceria)
+        {
+            climatizador = paramClimatizador;
+            tapiceria = paramTapiceria;
+        }
+
         private int ruedas;
         private double largo;
         private double ancho;
         private bool climatizador;
         private string tapiceria;
-
-
-
-
-
 
     }
 }
